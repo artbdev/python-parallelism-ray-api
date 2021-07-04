0efead2fb581ac5f0a366134d72808924fbe1a96
Arthur Parsons <artbdev@gmail.com>
2021-07-04 14:10:42 -0400

    Implement Knn Algo Python Multiproccessing
    * Delete knn multiproccessing implementation
    * Give knn ray implementation proper name

--- knn_algo_basic.py
+++ knn_algo_basic.py
@@ -1,22 +1,18 @@
 
 from numpy.lib.function_base import gradient
 import psutil
-import  ray
 import time
-from ray.worker import get
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from scipy.spatial import distance
 
 num_cpus = psutil.cpu_count(logical=False)
-ray.init(num_cpus=num_cpus, include_dashboard=False)
 
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
 
-@ray.remote
 def get_accuracy(y_test, predictions) -> int:
     distinct_elements = set(y_test).intersection(predictions)
     return (len(predictions) - len(distinct_elements)) / len(y_test)
@@ -27,7 +23,6 @@ def euclidean_distance(a,b) -> float:
 def fit(X_train, y_train) -> tuple:
     return X_train, y_train
 
-@ray.remote
 def predict(X_test):
     predictions = []
     for row in X_test:
@@ -47,15 +42,11 @@ def closest(row):
 
 start = time.time()
 
-predictions = predict.remote(X_test)
+predictions = predict(X_test)
 end = time.time()
 
 print("predict time taken ", (end - start))
+accuracy = get_accuracy(y_test, predictions)
 
-start = time.time()
-accuracy = get_accuracy.remote(y_test, predictions)
-end = time.time()
-print("accuracy calc time taken ", (end - start))
-
-print("Knn accuracy score: ", ray.get(accuracy))
+print("Knn accuracy score: ", accuracy)
 
